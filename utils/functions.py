2adb3834f34626eceb85ffd2c112473fa2fdb3c7
TJ Yanagitsuru <taro.yanagitsuru@gmail.com>
2024-04-07 23:08:55 +0900

    Added new updated functions by Anna and Christine

--- utils/functions.py
+++ utils/functions.py
@@ -3,19 +3,15 @@ import re
 import numpy as np
 import pandas as pd
 import openai
+import pandas as pd
+import itertools
 # import csv
 
+# Set API Key
 openkagi = st.secrets['key_a']
 
 ###helll00000"""
 
-
-# scores = {}
-# with open('Compatibility.csv') as csvfile:
-#     spamreader = csv.reader(csvfile, delimiter=',')
-#     next(spamreader)
-#     for a, b, score in spamreader:
-#         scores[a, b] = float(score)
 
 '''
 former get_ingredients_combinations, new compatibility function.
@@ -28,18 +24,7 @@ Output:
     ('chicken', 'mushroom')]
 
 '''
-import pandas as pd
-# filtered_df = pd.read_parquet('df.parquet.gzip')
 
-# # Vectorized operations to strip quotes and create tuples
-# ingredient1 = filtered_df['ingredient1'].str.strip("'")
-# ingredient2 = filtered_df['ingredient2'].str.strip("'")
-
-# # Use zip to pair elements and convert to set directly
-# verified_pairings = set(zip(ingredient1, ingredient2))
-
-
-import itertools
 def count_verified_pairings(combination, verified_pairings):
     count = 0
     for pair in itertools.combinations(combination, 2):
@@ -50,6 +35,8 @@ def count_verified_pairings(combination, verified_pairings):
 
 
 # Function to find the top 3 largest acceptable groups of ingredients
+# Input: user input (string) ingredients
+# Output: 3 ingredients combinations (list) candidates
 def find_top_3_groups(ingredients, verified_pairings):
 
     ingredients = [ingredient.strip() for ingredient in ingredients.split(',')]
@@ -322,3 +309,304 @@ def imagegen(title):
 # combinatins = muse_comb(df_comb)
 # recipes = gptrecipe(combinatins)
 # recipes
+
+
+
+
+
+
+
+
+
+####################################
+# Christine's code
+####################################
+
+
+
+def combinations_of_two(ingredients_input): ###dealt with the issue of missing space crash
+
+    '''
+    The function generates all unique pairs of ingredients that can be made from the input list of ingredients.
+    NOTE FOR FRONT-END: The output of this function is the input for data_query()
+    '''
+
+    ingredients_combinations = []
+    powerpowerset = []
+    ingredients = re.split(r',', ingredients_input.strip())
+    ingredients_list = list(set(ingredient.strip() for ingredient in ingredients))
+    for r in range(len(ingredients_list)+1):
+        combinations = itertools.combinations(ingredients_list, r)
+        #powerset.extend(subset for subset in combinations if len(subset) > 1)
+        for comb in combinations:
+            if len(comb) > 1:
+                if len(comb) < 3:
+                    ingredients_combinations.append(comb)
+                else:
+                    powerpowerset.append(comb)
+                    for power in powerpowerset:
+                        lowerset = []
+                        combins = itertools.combinations(power, 2)
+                        for arrange in combins:
+                            lowerset.append(arrange)
+                    ingredients_combinations.append(lowerset)
+    return ingredients_combinations
+
+'''-----------------------------------------------------------------------------------------------------------'''
+
+def data_query(df, ingredients_combinations): ##Added a penalty of -5 for pairings that are not in the dataframe
+    """
+    INPUT: get_dataframe(), combinations_of_two()
+    NOTE FOR FRONT-END: The output of this function is the input for muse_comb()
+    """
+    data = []
+    for combination in ingredients_combinations:
+        if len(combination) < 3:
+            ingredient1, ingredient2 = combination
+            query_str = f'(ingredient1 == "{ingredient1}" & ingredient2 == "{ingredient2}") | (ingredient1 == "{ingredient2}" & ingredient2 == "{ingredient1}")'
+            score = df.query(query_str)['scaled_col'].values
+            if len(score) > 0:
+                data.append({'Combination': combination, 'Score': score})
+            else:
+                continue
+        else:
+            scores = []
+            for i in combination:
+                ingredient1, ingredient2 = i
+                query_str = f'(ingredient1 == "{ingredient1}" & ingredient2 == "{ingredient2}") | (ingredient1 == "{ingredient2}" & ingredient2 == "{ingredient1}")'
+                score = df.query(query_str)['scaled_col'].values
+                if len(score) > 0:
+                    scores.append(score[0])
+                else:
+                    scores.append(-5)
+            data.append({'Combination': combination, 'Score': scores})
+
+    df_comb = pd.DataFrame(data)
+    return df_comb
+
+'''-----------------------------------------------------------------------------------------------------------'''
+def get_dataframe(file):
+    """reads the parquet.gzip file ["Halved-DF.parquet.gzip"]
+    NOTE FOR FRON-END: the output of this function is an input for data_query()
+    """
+    df = pd.read_parquet(file)
+
+    return df
+'''-----------------------------------------------------------------------------------------------------------'''
+def final_recipes(recipes, scores):  ###<=== Function for evaluating if the score passes the threshold and regenerating if it doesn't
+    """
+    This evaluates whether the score of a recipe passes or fails the threshold.
+    If the recipe doesn't meet the threshold after 3 attempts, the last generated recipe is added.
+    INPUT: Output of the recipe generator function
+    NOTE FOR FRONT-END: it's important to make sure that the outputs of the new recipe generator are the same as the
+                        old version for this function to still work.
+                        optimized_gptrecipe() and scoring_model() must be replaced with the actual functions
+    """
+    final_recipes = {"Title": [], "Ingredients": [], "Instructions": []}
+    threshold = 2
+
+    for i in range(len(recipes["Title"])):
+        if scores[i] >= threshold:
+            final_recipes["Title"].append(recipes["Title"][i])
+            final_recipes["Ingredients"].append(recipes["Ingredients"][i])
+            final_recipes["Instructions"].append(recipes["Instructions"][i])
+        else:
+            n = 0
+            tmp_recipe = {
+                "Title":recipes["Title"][i],
+                "Ingredients":recipes["Ingredients"][i],
+                "Instructions":recipes["Instructions"][i]
+                         }
+            last_recipe = {
+                "Title":recipes["Title"][i],
+                "Ingredients":recipes["Ingredients"][i],
+                "Instructions":recipes["Instructions"][i]
+                         }
+            while n < 3:
+                new_recipe = optimized_gptrecipe(tmp_recipe["Ingredients"][0]) ###<=== insert actual recipe generator
+                new_score = scoring_model(new_recipe["Instructions"][0]) ###<=== insert the actual scoring model function here
+                if new_score >= threshold:
+                    final_recipes["Title"].append(new_recipe["Title"])
+                    final_recipes["Ingredients"].append(new_recipe["Ingredients"])
+                    final_recipes["Instructions"].append(new_recipe["Instructions"])
+                    break  # Exit loop if the new recipe passes the threshold
+                else:
+                    last_recipe = new_recipe  # Update tmp_recipe with the new recipe if the threshold isn't met
+                    n += 1
+            else: # Add the last generated recipe if the loop completes without finding a passing recipe
+                final_recipes["Title"].append(last_recipe["Title"])
+                final_recipes["Ingredients"].append(last_recipe["Ingredients"])
+                final_recipes["Instructions"].append(last_recipe["Instructions"])
+                break  # Exit the outer loop to prevent an unending loop
+
+    return final_recipes
+
+
+'''-----------------------------------------------------------------------------------------------------------'''
+def muse_comb(data_query_df): ###If this takes too long, consider taking the nested calculate_sum(array) outside of the function
+    '''
+     the function calculates the sum of the "Score" values and returns the three combinations with the largest sums
+     OUTPUT: [['yeast', 'butter', 'eggs', 'pepper', 'cabbage', 'pork', 'flour', 'sugar'],
+                 ['butter', 'eggs', 'pepper', 'cabbage', 'pork', 'flour', 'sugar'],
+                 ['yeast', 'butter', 'eggs', 'pepper', 'cabbage', 'flour', 'sugar']]
+
+     NOTE FOR FRONT-END: The return is a list of lists so access the values by indexing e.g. output[0]
+
+                         The output of this function is the input for the recipe generator
+
+                         We might need a function to convert each lists into strings if
+                         the recipe generator doesn't do this automatically.
+    '''
+
+    def calculate_sum(array):
+        return sum(array)
+
+    def ingredients_to_lists(lists):
+        ingredients_list = []
+        for i in range(3):
+            tmp_list = []
+            for x in lists[i]:
+                tmp_list.append(x[0])
+                tmp_list.append(x[1])
+            ingredients_list.append(list(set(tmp_list)))
+
+        return ingredients_list
+
+    for i in range(len(data_query_df)):
+        data_query_df["Sum"] = data_query_df["Score"].apply(calculate_sum)
+
+    max_values = data_query_df.nlargest(3, "Sum")
+
+    max_values = max_values["Combination"].reset_index(drop=True)
+
+    ingredients_lists = ingredients_to_lists(max_values)
+
+    return ingredients_lists
+
+'''--------------------------------------------------------------------------------------------------------------'''
+
+
+
+
+
+
+
+
+####################################
+# Anna's code
+####################################
+
+
+
+
+from transformers import FlaxAutoModelForSeq2SeqLM
+from transformers import AutoTokenizer
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from gradio_client import Client
+
+def recipe_generator(ingredients):
+    MODEL_NAME_OR_PATH = "flax-community/t5-recipe-generation"
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME_OR_PATH, use_fast=True)
+    model = FlaxAutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME_OR_PATH)
+
+    prefix = "items: "
+    generation_kwargs = {
+        "max_length": 512,
+        "min_length": 64,
+        "no_repeat_ngram_size": 3,
+        "do_sample": True,
+        "top_k": 60,
+        "top_p": 0.95
+    }
+    special_tokens = tokenizer.all_special_tokens
+    tokens_map = {
+        "<sep>": "--",
+        "<section>": "\n"
+    }
+    def skip_special_tokens(text, special_tokens):
+        for token in special_tokens:
+            text = text.replace(token, "")
+
+        return text
+
+    def target_postprocessing(texts, special_tokens):
+        if not isinstance(texts, list):
+            texts = [texts]
+
+        new_texts = []
+        for text in texts:
+            text = skip_special_tokens(text, special_tokens)
+
+            for k, v in tokens_map.items():
+                text = text.replace(k, v)
+
+            new_texts.append(text)
+
+        return new_texts
+
+    def generation_function(texts):
+        _inputs = texts if isinstance(texts, list) else [texts]
+        inputs = [prefix + inp for inp in _inputs]
+        inputs = tokenizer(
+            inputs,
+            max_length=256,
+            padding="max_length",
+            truncation=True,
+            return_tensors="jax"
+        )
+
+        input_ids = inputs.input_ids
+        attention_mask = inputs.attention_mask
+
+        output_ids = model.generate(
+            input_ids=input_ids,
+            attention_mask=attention_mask,
+            **generation_kwargs
+        )
+        generated = output_ids.sequences
+        generated_recipe = target_postprocessing(
+            tokenizer.batch_decode(generated, skip_special_tokens=False),
+            special_tokens
+        )
+        return generated_recipe
+    recipe = generation_function(ingredients)
+    return recipe
+
+
+def convert_to_dictionary(recipes):
+    recipe_dicts = []
+    for recipe in recipes:
+        recipe_dict = {}
+        parts = recipe.split('\n')
+        for part in parts:
+            key, value = part.split(':', 1)
+            key = key.strip().upper()
+            value = value.strip()
+
+            if key in ['INGREDIENTS', 'DIRECTIONS']:
+                items = [f"{i+1}. {info.strip().capitalize()}" for i, info in enumerate(value.split("--"))]
+                recipe_dict[key] = "\n".join(items)
+            else:
+                recipe_dict[key] = value
+        recipe_dicts.append(recipe_dict)
+
+    return recipe_dicts
+
+
+def image_generator(recipe):
+    client = Client("https://playgroundai-playground-v2-5.hf.space/--replicas/o9oxl/")
+    result = client.predict(
+            recipe,
+            " ",
+            False,
+            820,
+            1024,
+            1024,
+            3,
+            True,
+            api_name="/run"
+    )
+
+    image_path = result[0][0]['image']
+    return image_path
